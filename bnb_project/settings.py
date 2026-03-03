5851e7fd62d199fe6fccfa05577632108ce83fe0
Iain <iain.kirkham@outlook.com>
2026-03-03 15:14:37 +0000

    feat: update templates and URLs for room detail and booking views

--- bnb_project/settings.py
+++ bnb_project/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'bnb_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'bnb_project' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Room, Booking
 from .forms import BookingForm
 
 
 class RoomListView(ListView):
     model = Room
-    template_name = 'rooms/room_list.html'
+    template_name = 'home/index.html'
     context_object_name = 'rooms'
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'room_detail/room_detail.html'
+    context_object_name = 'room'
 
 
 def book_room(request, room_id):
@@ -23,7 +29,7 @@ def book_room(request, room_id):
     else:
         form = BookingForm()
 
-    return render(request, "rooms/booking.html", {
+    return render(request, "booking/booking.html", {
         "room": room,
         "form": form,
     })
@@ -31,6 +37,6 @@ def book_room(request, room_id):
 
 def booking_confirmation(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
-    return render(request, "rooms/booking_confirmation.html", {
+    return render(request, "booking/booking.html", {
         "booking": booking,
     })
