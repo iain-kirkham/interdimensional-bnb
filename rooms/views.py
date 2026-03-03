a087b462b25448a82c552e9a754592412fd04324
Iain <iain.kirkham@outlook.com>
2026-03-03 10:50:39 +0000

    feat: implement Room model and corresponding views and URLs

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Room
 
-# Create your views here.
+class RoomListView(ListView):
+    model = Room
+    template_name = 'rooms/room_list.html'
+    context_object_name = 'rooms'
